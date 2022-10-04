7b3ff051331842833820c10804406292fa4a77a9
sega6300 <sega6300@gmail.com>
2022-10-04 22:21:47 +0500

    Add some simple tests

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -1,20 +1,83 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.IO;
 using Xunit;
 namespace CreditCards.UITests
 {
     public class CreditCardWebAppShould
     {
+        public const string HomePageUrl = "http://localhost:44108/";
+        public const string AboutPageUrl = "http://localhost:44108/Home/About";
+        public const string HomePageTitle = "Home Page - Credit Cards";
+
         [Fact]
         [Trait("Categoty", "Smoke")]
         public void LoadApplicationPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("http://localhost:44108/");
-            }
+                driver.Navigate().GoToUrl(HomePageUrl);
+              
+                DemoHelper.Pause();               
 
+                Assert.Equal(HomePageTitle, driver.Title);
+                Assert.Equal(HomePageUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Categoty", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomePageUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Refresh();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomePageTitle, driver.Title);
+                Assert.Equal(HomePageUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Categoty", "Smoke")]
+        public void ReloadHomePageBack()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(AboutPageUrl);
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(HomePageUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+                driver.Navigate().Forward();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomePageTitle, driver.Title);
+                Assert.Equal(HomePageUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Categoty", "Smoke")]
+        public void ReloadHomePageForward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomePageUrl);
+                DemoHelper.Pause();
+                driver.Navigate().GoToUrl(AboutPageUrl);
+                DemoHelper.Pause();
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                Assert.Equal(HomePageTitle, driver.Title);
+                Assert.Equal(HomePageUrl, driver.Url);
+            }
         }
     }
 }
---
+++ CreditCards.UITests/DemoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace CreditCards.UITests
+{
+    internal static class DemoHelper
+    {
+        /// <summary>
+        /// Thread pause
+        /// </summary>
+        /// <param name="timeToSleep">Thead time sleep, ms</param>
+        public static void Pause(int timeToSleep= 3000)
+        {
+            Thread.Sleep(timeToSleep);
+        }
+    }
+}
